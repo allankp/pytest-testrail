aa5b1ba8cccb92012b05cb0ab0bd35b7bb46889b
Chris Hartjes <chartjes@littlehart.net>
2016-09-13 11:09:49 -0400

    Reverted some formatting changes at @allankilpatrick's request, code now passed flake8 with local settings

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -29,8 +29,7 @@ def api_client():
 
 @pytest.fixture
 def tr_plugin(api_client):
-    return TestRailPlugin(
-        api_client, ASSIGN_USER_ID, PROJECT_ID, SUITE_ID, True)
+    return TestRailPlugin(api_client, ASSIGN_USER_ID, PROJECT_ID, SUITE_ID, True)
 
 
 @pytest.fixture
@@ -41,13 +40,11 @@ def pytest_test_items(testdir):
 
 @freeze_time(FAKE_NOW)
 def test_testrun_name():
-    run_time = FAKE_NOW.strftime(plugin.DT_FORMAT)
-    assert plugin.testrun_name() == 'Automated Run {}'.format(run_time)
+    assert plugin.testrun_name() == 'Automated Run {}'.format(FAKE_NOW.strftime(plugin.DT_FORMAT))
 
 
 def test_failed_outcome(tr_plugin):
-    failed_outcome = plugin.PYTEST_TO_TESTRAIL_STATUS['failed']
-    assert plugin.get_test_outcome('failed') == failed_outcome
+    assert plugin.get_test_outcome('failed') == plugin.PYTEST_TO_TESTRAIL_STATUS['failed']
 
 
 def test_successful_outcome(tr_plugin):
@@ -82,9 +79,7 @@ def test_add_result(tr_plugin):
 
 def pytest_runtest_makereport(pytest_test_items, tr_plugin):
     keys = ['C4354', 'C1234']
-    report = Mock(
-        keywords={key: None for key in keys},
-        failed=True, when='teardown')
+    report = Mock(keywords={key: None for key in keys}, failed=True, when='teardown')
 
     tr_plugin.pytest_runtest_makereport(report)
 
@@ -111,19 +106,14 @@ def test_pytest_sessionfinish(api_client, tr_plugin):
     expected_uri = plugin.ADD_RESULTS_URL.format(10)
     expected_data = {'results': [1, 2]}
     check_cert = True
-    api_client.send_post.assert_called_once_with(
-        expected_uri,
-        expected_data,
-        check_cert
-    )
+    api_client.send_post.assert_called_once_with(expected_uri, expected_data, check_cert)
 
 
 def test_create_test_run(api_client, tr_plugin):
     expected_tr_keys = [3453, 234234, 12]
     expect_name = 'testrun_name'
 
-    tr_plugin.create_test_run(
-        ASSIGN_USER_ID, PROJECT_ID, SUITE_ID, expect_name, expected_tr_keys)
+    tr_plugin.create_test_run(ASSIGN_USER_ID, PROJECT_ID, SUITE_ID, expect_name, expected_tr_keys)
 
     expected_uri = plugin.ADD_TESTRUN_URL.format(PROJECT_ID)
     expected_data = {
@@ -134,7 +124,4 @@ def test_create_test_run(api_client, tr_plugin):
         'case_ids': expected_tr_keys
     }
     check_cert = True
-    api_client.send_post.assert_called_once_with(
-        expected_uri,
-        expected_data, check_cert
-    )
+    api_client.send_post.assert_called_once_with(expected_uri, expected_data, check_cert)
