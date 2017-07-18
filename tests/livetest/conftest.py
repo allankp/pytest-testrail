61d07f852b6f41a44c70e127d8627b6c954bbca4
apallier <a.pallier@laposte.net>
2017-07-18 14:54:38 +0200

    Move live tests in ' tests' folder and configure a conftest.py to allow their executions

---
+++ tests/livetest/conftest.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+import sys
+import os.path
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, os.pardir))
+
+from pytest_testrail.conftest import *
