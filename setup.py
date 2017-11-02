a2893828cbd01ebd4730677287eb1577b0d198ae
Andrea Ratto <aratto@topcon.com>
2017-11-02 12:56:07 +0100

    Version 1.0.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name='pytest-testrail',
     description='pytest plugin for creating TestRail runs and adding results',
     long_description=read_file('README.rst'),
-    version='1.0.0',
+    version='1.0.1',
     author='Allan Kilpatrick',
     author_email='allanklp@gmail.com',
     url='http://github.com/allankilpatrick/pytest-testrail/',
