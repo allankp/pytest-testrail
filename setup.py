6ee5bf8176f73bc57762fa6e7a40d473c838bcd8
Allan Kilpatrick <allan.kilpatrick@postcodelottery.co.uk>
2018-10-26 14:25:16 +0100

    bump for readme fixes

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name='pytest-testrail',
     description='pytest plugin for creating TestRail runs and adding results',
     long_description=read_file('README.rst'),
-    version='2.2.0',
+    version='2.3.0',
     author='Allan Kilpatrick',
     author_email='allanklp@gmail.com',
     url='http://github.com/allankp/pytest-testrail/',
