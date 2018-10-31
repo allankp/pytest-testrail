875ad200525c8afa0477b8a9eda1b95590892243
Allan Kilpatrick <dubnerr@gmail.com>
2018-10-31 11:23:53 +0000

    Requests version bump (#76)
    * requests 2.20.0 required
    
    * Bump version: 2.3.0 → 2.3.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name='pytest-testrail',
     description='pytest plugin for creating TestRail runs and adding results',
     long_description=read_file('README.rst'),
-    version='2.3.0',
+    version='2.3.1',
     author='Allan Kilpatrick',
     author_email='allanklp@gmail.com',
     url='http://github.com/allankp/pytest-testrail/',
@@ -20,7 +20,7 @@ setup(
     package_dir={'pytest_testrail': 'pytest_testrail'},
     install_requires=[
         'pytest>=2',
-        'requests>=2',
+        'requests>=2.20.0',
         'simplejson',
     ],
     include_package_data=True,
